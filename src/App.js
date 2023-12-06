9404e217be51210804804d2efcbc4e652d907829
aabhishek777 <aabhishekksinghh@gmail.com>
2023-12-06 15:20:18 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Admin from './components/Admin';
-import ShowAdmin from './components/ShowAdmin';
-import ShowBook from './components/ShowBooks';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Admin from "./components/Admin";
+import ShowAdmin from "./components/ShowAdmin";
+import ShowBook from "./components/ShowBooks";
 
 function App() {
   const [bookshelf, setBookshelf] = useState([]);
 
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/admin"
-          render={() => <Admin bookshelf={bookshelf} setBookshelf={setBookshelf} />}
-        />
-        <Route
-          path="/show-admin"
-          render={() => <ShowAdmin bookshelf={bookshelf} setBookshelf={setBookshelf} />}
-        />
-        <Route
-          path="/show-book/:isbn"
-          render={() => <ShowBook bookshelf={bookshelf} />}
-        />
-      </Switch>
-    </Router>
+    <Routes>
+      <Route
+        path="/admin"
+        element={<Admin bookshelf={bookshelf} setBookshelf={setBookshelf} />}
+      />
+      <Route
+        path="/show-admin"
+        element={
+          <ShowAdmin bookshelf={bookshelf} setBookshelf={setBookshelf} />
+        }
+      />
+      <Route
+        path="/show-book/:isbn"
+        element={<ShowBook bookshelf={bookshelf} setBookshelf={setBookshelf} />}
+      />
+    </Routes>
   );
 }
 
